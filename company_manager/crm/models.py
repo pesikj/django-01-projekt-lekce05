75b295cd25aac1323296175ec3d8a7f2cd4d60a7
Jiří Pešík <jiri.pesik@outlook.cz>
2022-05-01 20:27:36 +0200

    Přidáno pole value

--- company_manager/crm/models.py
+++ company_manager/crm/models.py
@@ -50,6 +50,7 @@ class Opportunity(models.Model):
     primary_contact = models.ForeignKey(Contact, on_delete=models.SET_NULL, null=True, blank=True)
     description = models.TextField(null=True)
     status = models.CharField(max_length=2, default="1", choices=status_choices)
+    value = models.DecimalField(max_digits=10, decimal_places=2, null=True)
 
 class Employee(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
